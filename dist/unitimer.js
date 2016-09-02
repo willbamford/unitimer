960ce220441ed23b51c066b3f04928a7762a361e
Will Bamford <w.bamford@gmail.com>
2016-09-02 21:21:09 +0100

    Working implementation albeit basic

--- dist/unitimer.js
+++ dist/unitimer.js
@@ -1,4 +1,31 @@
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.unitimer = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+function now () {
+  return typeof window.performance !== 'undefined' &&
+    typeof window.performance.now !== 'undefined'
+      ? window.performance.now()
+      : Date.now()
+}
 
-},{}]},{},[1])(1)
+module.exports = now
+
+},{}],2:[function(require,module,exports){
+var now = require('./now')
+
+var starts = {}
+
+var unitimer = {
+  start: function (label) {
+    starts[label] = now()
+  },
+  stop: function (label) {
+    if (starts[label]) {
+      return now() - starts[label]
+    }
+    return -1
+  }
+}
+
+module.exports = unitimer
+
+},{"./now":1}]},{},[2])(2)
 });
--- test/index.js
+++ test/index.js
@@ -1,34 +1,57 @@
 const test = require('ava')
-const proxyquire = require('proxyquire')
-// const timer = require('../lib')
+const sinon = require('sinon')
+const timer = require('../lib')
 
-function getUnitimer () {
-  return proxyquire('../lib', {
-    './hrtime': () => {
-      return [2, 123456789]
-    }
-  })
-}
-
-test('start is a function', (t) => {
-  const timer = getUnitimer()
-  t.is(typeof timer.start, 'function')
-})
-
-test('end is a function', (t) => {
-  const timer = getUnitimer()
-  t.is(typeof timer.end, 'function')
-})
-
-test('return -1 if start label does not exist', (t) => {
-  const timer = getUnitimer()
-  const millis = timer.end('does-not-exist')
+test('start label does not exist', (t) => {
+  const millis = timer.stop('does-not-exist')
   t.is(millis, -1)
 })
 
-test('measure time taken between start and end', (t) => {
-  const timer = getUnitimer()
-  timer.start('label')
-  const millis = timer.end('label', 'ms')
-  t.is(millis, 2123.456789)
+test('start stop intervals', (t) => {
+  const times = [
+    [4, 10],
+    [10, 50],
+    [9, 999999999],
+    [10, 0]
+  ]
+  let index = 0
+  function fn () {
+    return times[index++]
+  }
+  var hrtime = sinon.stub(process, 'hrtime', fn)
+  timer.start('A')
+  const msA = timer.stop('A')
+  t.is(msA, 6000.000040000001) // 6000.00004
+  timer.start('B')
+  const msB = timer.stop('B')
+  t.is(msB, 0.0000010000003385357559) // 0.000001
+  hrtime.restore()
+})
+
+test('overlapping intervals', (t) => {
+  const times = [
+    [4, 10],      // A0
+    [4, 11],      // B0
+    [10, 50],     // A1
+    [22, 100],    // C0
+    [23, 200],    // C1
+    [30, 11]      // B1
+  ]
+  let index = 0
+  function fn () {
+    return times[index++]
+  }
+  var hrtime = sinon.stub(process, 'hrtime', fn)
+
+  timer.start('A')
+  timer.start('B')
+  const msA = timer.stop('A')
+  timer.start('C')
+  const msC = timer.stop('C')
+  const msB = timer.stop('B')
+  t.is(msA, 6000.000040000001) // 6000.00004
+  t.is(msB, 26000)
+  t.is(msC, 1000.0000999999975) // 1000.000100
+
+  hrtime.restore()
 })
